59e2616627d9286c361679f364108028c321e688
Paul Sanderson <red010b37@users.noreply.github.com>
2018-04-18 19:28:06 +1200

    Comments out curls

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 
 cd /
 
-curl -X POST -H 'Content-Type: application/json' -d "About to clone" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "About to clone" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 git clone -b v4.1.1 https://github.com/NAVCoin/navcoin-core.git
 
-curl -X POST -H 'Content-Type: application/json' -d "clone complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "clone complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 cd /navcoin-core
 
-curl -X POST -H 'Content-Type: application/json' -d "cd to navcoin-core" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "cd to navcoin-core" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 ./autogen.sh
 
-curl -X POST -H 'Content-Type: application/json' -d "ran autogen" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "ran autogen" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 ./configure LDFLAGS="-L/usr/local/berkeley-db-4.8/lib/" \
@@ -30,9 +30,9 @@ curl -X POST -H 'Content-Type: application/json' -d "About to Make" https://webh
 ls
 make
 
-curl -X POST -H 'Content-Type: application/json' -d "Make Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "Make Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 make install
-curl -X POST -H 'Content-Type: application/json' -d "Install Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "Install Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 ls
 cd ..
@@ -40,21 +40,21 @@ cd ..
 rm -fr /navcoin-core/*
 rm -r /navcoin-core/
 
-curl -X POST -H 'Content-Type: application/json' -d "rm navcoin-src Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "rm navcoin-src Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 navcoind -testnet -rpcuser=hi -rpcpassword=pass &
-curl -X POST -H 'Content-Type: application/json' -d "Start navcoin core Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "Start navcoin core Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
-curl -X POST -H 'Content-Type: application/json' -d "Sleeping" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "Sleeping" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 sleep 5
-curl -X POST -H 'Content-Type: application/json' -d "awake" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "awake" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 navcoin-cli -testnet -rpcuser=hi -rpcpassword=pass addnode "176.9.19.245" "add"
 navcoin-cli -testnet -rpcuser=hi -rpcpassword=pass addnode "46.4.24.136" "add"
 
-curl -X POST -H 'Content-Type: application/json' -d "add nodes complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "add nodes complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 OUTPUT="$(navcoin-cli -testnet -staking -rpcuser=hi -rpcpassword=pass listreceivedbyaddress 0 true)"
@@ -63,7 +63,7 @@ echo "${OUTPUT}"
 curl -X POST -H 'Content-Type: application/json' -d "${OUTPUT}" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
-curl -X POST -H 'Content-Type: application/json' -d "Finished" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
+#curl -X POST -H 'Content-Type: application/json' -d "Finished" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
 sleep 9999999999
