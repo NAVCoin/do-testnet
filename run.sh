361ed7c507cc0dbc3c4c39f557391c36a6bc9ced
Paul Sanderson <paul@creativetech.io>
2018-04-18 17:41:34 +1200

    updates run

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-cd ~
+cd /
 
 curl -X POST -H 'Content-Type: application/json' -d "About to clone" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
@@ -8,7 +8,7 @@ git clone -b v4.1.1 https://github.com/NAVCoin/navcoin-core.git
 curl -X POST -H 'Content-Type: application/json' -d "clone complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 
-cd navcoin-core
+cd /navcoin-core
 
 curl -X POST -H 'Content-Type: application/json' -d "cd to navcoin-core" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
@@ -31,17 +31,15 @@ ls
 make
 
 curl -X POST -H 'Content-Type: application/json' -d "Make Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
-OUTPUT="$(make install)"
-curl -X POST -H 'Content-Type: application/json' -d "${OUTPUT}" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
-
+make install
 
 curl -X POST -H 'Content-Type: application/json' -d "Install Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
 ls
 cd ..
 
-rm -fr navcoin-core/*
-rm -r navcoin-core/
+rm -fr /navcoin-core/*
+rm -r /navcoin-core/
 
 curl -X POST -H 'Content-Type: application/json' -d "rm navcoin-src Complete" https://webhook.site/eeb8307f-67ce-460b-9a87-24f9f7575d48
 
